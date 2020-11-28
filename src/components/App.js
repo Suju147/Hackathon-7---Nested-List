19f6c2c8c496b33ac68b73b44c8fa22191356b36
sujata chandravanshi <sujatachandravanshi@gmail.com>
2020-11-28 18:55:01 +0530

    next

--- src/components/App.js
+++ src/components/App.js
@@ -34,7 +34,8 @@ const states = [
         name: "Gwadivor",
         towns: [
           {
-            name: "Ajaypur",
+            name: "Ajaypur"},
+            {name: "cool"
           },
         ],
       },
@@ -159,51 +160,132 @@ const states = [
 function App() {
   const cityfun=(city)=>{
     console.log(city.target);
-    if(city.target.id="city1")
-    {
+    if(city.target.className==="state1")
+    {if(city.target.id==="city1") 
+    {console.log("1");
       ReactDOM.render((<div>
         <div id="town1">{states[0].cities[0].towns[0].name}</div>
         <div id="town2">{states[0].cities[0].towns[1].name}</div>
       </div>),document.getElementById("city1"));
     }
-    else if(city.target.id="city2")
-    {
+    else if(city.target.id==="city2")
+    {console.log("2")
       ReactDOM.render((<div>
         <div id="town1">{states[0].cities[1].towns[0].name}</div>
         <div id="town2">{states[0].cities[1].towns[1].name}</div>
       </div>),document.getElementById("city2"));}
-     else if(city.target.id="city3")
+
+     else if(city.target.id==="city3")
       {
+        console.log("3");
         ReactDOM.render((<div>
           <div id="town1">{states[0].cities[2].towns[0].name}</div>
           <div id="town2">{states[0].cities[2].towns[1].name}</div>
         </div>),document.getElementById("city3"));
+      }}
+      else if(city.target.className==="state2")
+      {
+        if(city.target.id==="city1") 
+    {console.log("1");
+      ReactDOM.render((<div>
+        <div id="town1">{states[1].cities[0].towns[0].name}</div>
+        <div id="town2">{states[1].cities[0].towns[1].name}</div>
+      </div>),document.getElementById("city1"));
+    }
+    else if(city.target.id==="city2")
+    {console.log("2")
+      ReactDOM.render((<div>
+        <div id="town1">{states[1].cities[1].towns[0].name}</div>
+        <div id="town2">{states[1].cities[1].towns[1].name}</div>
+      </div>),document.getElementById("city2"));}
+
+     else if(city.target.id==="city3")
+      {
+        console.log("3");
+        ReactDOM.render((<div>
+          <div id="town1">{states[1].cities[2].towns[0].name}</div>
+          <div id="town2">{states[1].cities[2].towns[1].name}</div>
+        </div>),document.getElementById("city3"));
+      }
+
+      }
+
+      else if(city.target.className==="state3")
+      {
+        if(city.target.id==="city1") 
+    {console.log("1");
+      ReactDOM.render((<div>
+        <div id="town1">{states[2].cities[0].towns[0].name}</div>
+        <div id="town2">{states[2].cities[0].towns[1].name}</div>
+      </div>),document.getElementById("city1"));
+    }
+    else if(city.target.id==="city2")
+    {console.log("2")
+      ReactDOM.render((<div>
+        <div id="town1">{states[2].cities[1].towns[0].name}</div>
+        <div id="town2">{states[2].cities[1].towns[1].name}</div>
+      </div>),document.getElementById("city2"));}
+
+     else if(city.target.id==="city3")
+      {
+        console.log("3");
+        ReactDOM.render((<div>
+          <div id="town1">{states[2].cities[2].towns[0].name}</div>
+          <div id="town2">{states[2].cities[2].towns[1].name}</div>
+        </div>),document.getElementById("city3"));
+      }
+      }
+
+      else if(city.target.className==="state4")
+      {
+        if(city.target.id==="city1") 
+    {console.log("1");
+      ReactDOM.render((<div>
+        <div id="town1">{states[3].cities[0].towns[0].name}</div>
+        <div id="town2">{states[3].cities[0].towns[1].name}</div>
+      </div>),document.getElementById("city1"));
+    }
+    else if(city.target.id==="city2")
+    {console.log("2")
+      ReactDOM.render((<div>
+        <div id="town1">{states[3].cities[1].towns[0].name}</div>
+        <div id="town2">{states[3].cities[1].towns[1].name}</div>
+      </div>),document.getElementById("city2"));}
+
+     else if(city.target.id==="city3")
+      {
+        console.log("3");
+        ReactDOM.render((<div>
+          <div id="town1">{states[3].cities[2].towns[0].name}</div>
+          <div id="town2">{states[3].cities[2].towns[1].name}</div>
+        </div>),document.getElementById("city3"));
+      }
       }
     
   }
 
   const statefun=(state)=>{
     if(state.target.id==='state1')
-    {console.log("10");
+    {console.log(state.target);
     ReactDOM.render(
-      (<div ><div id="city1" onClick={cityfun}>{states[0].cities[0].name}</div>
-    <div id="city2" onClick={cityfun}>{states[0].cities[1].name}</div>
-    <div id="city3" onClick={cityfun}>{states[0].cities[2].name}</div></div>),document.getElementById("state1"));
+      (<div id="1"><div id="city1" className="state1" onClick={cityfun}>{states[0].cities[0].name}</div>
+    <div id="city2" className="state1" onClick={cityfun}>{states[0].cities[1].name}</div>
+    <div id="city3" className="state1" onClick={cityfun}>{states[0].cities[2].name}</div></div>),document.getElementById("state1"));
     }
     else if(state.target.id==='state2')
-    {ReactDOM.render((<div ><div id="city1" onClick={cityfun}>{states[1].cities[0].name}</div>
-    <div id="city2" onClick={cityfun}>{states[1].cities[1].name}</div>
-    <div id="city3" onClick={cityfun}>{states[1].cities[2].name}</div></div>),document.getElementById("state2"));
+    {ReactDOM.render((<div id="2"><div id="city1" className="state2" onClick={cityfun}>{states[1].cities[0].name}</div>
+    <div id="city2" className="state2" onClick={cityfun}>{states[1].cities[1].name}</div>
+    <div id="city3" className="state2" onClick={cityfun}>{states[1].cities[2].name}</div></div>),document.getElementById("state2"));
     }
     else if(state.target.id==='state3')
-    {ReactDOM.render((<div ><div id="city1" onClick={cityfun}>{states[2].cities[0].name}</div>
-    <div id="city2" onClick={cityfun}>{states[2].cities[1].name}</div>
-    <div id="city3" onClick={cityfun}>{states[2].cities[2].name}</div></div>),document.getElementById("state3"));}
+    {ReactDOM.render((<div id="3"><div id="city1" className="state3" onClick={cityfun}>{states[2].cities[0].name}</div>
+    <div id="city2"className="state3" onClick={cityfun}>{states[2].cities[1].name}</div>
+    <div id="city3"  className="state3" onClick={cityfun}>{states[2].cities[2].name}</div></div>),document.getElementById("state3"));}
 
     else if(state.target.id==='state4')
-    {ReactDOM.render((<div ><div id="city1" onClick={cityfun}>{states[3].cities[0].name}</div>
-    <div id="city2" onClick={cityfun}>{states[3].cities[1].name}</div>
-    <div id="city3"onClick={cityfun}>{states[3].cities[2].name}</div></div>),document.getElementById("state3"));
+    {ReactDOM.render((<div id="4"><div id="city1" className="state4" onClick={cityfun}>{states[3].cities[0].name}</div>
+    <div id="city2" className="state4" onClick={cityfun}>{states[3].cities[1].name}</div>
+    <div id="city3"className="state4" onClick={cityfun}>{states[3].cities[2].name}</div></div>),document.getElementById("state4"));
     }
     
   }
